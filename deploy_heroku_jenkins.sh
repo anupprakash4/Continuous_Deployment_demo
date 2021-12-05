d105d73e3e06a2747c1ae720ab71954e2defb081
anupprakash4 <88568799+anupprakash4@users.noreply.github.com>
2021-12-05 23:42:15 +0530

    Update deploy_heroku_jenkins.sh

--- deploy_heroku_jenkins.sh
+++ deploy_heroku_jenkins.sh
@@ -2,9 +2,9 @@
 set -x
 heroku container:login
 #login to container
-heroku container:push web -a democicdapp
+heroku container:push web -a democicdapp2
 #push the docker image
-heroku container:release web -a democicdapp
+heroku container:release web -a democicdapp2
 # release the application
-heroku info -a democicdapp
+heroku info -a democicdapp2
 #get info about application
